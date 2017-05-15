f58158066ec19ffb32bbb7bcaaf37303a01cffe6
ezmurphy <elizabethaziegler@gmail.com>
2017-05-14 23:38:01 -0700

    Added includes folder
    Includes for nav, footer grid and footer copyright

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<div class='copy container center-block clearfix'>
+            <p class='text-center'>&copy; 2017-" .date("Y") .  " Ace in the Hole Multisport Events</p> 
+</div>";
+
+?>
