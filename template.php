fab379ed42ab50e5c665fbe154bdca04699ac210
ezmurphy <elizabethaziegler@gmail.com>
2017-05-14 23:43:43 -0700

    Uploaded template.php
    This is the template for the Ace in the Hole Multisport Events project.

---
+++ template.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+    <head>
+        
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
+    <title>Ace in the Hole Multisport Events Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        
+    <!-- Custom styles for Bootstrap static top navbar -->
+    <link href="css/navbar-static-top.css" rel="stylesheet">
+    
+    <!-- Custom styles for Bootstrap starter template -->
+    <link href="css/starter-template.css" rel="stylesheet"> 
+        
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet">
+        
+    <!-- Weather Plugin CSS -->
+    <link href="css/simpleWeather.css" rel="stylesheet">
+        
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    </head>
+  
+    <body id="top">
+        
+    <?php include_once 'includes/nav.html';?>
+      
+    <main>
+        
+    <header>
+        
+        <div class="tagline starter-template">
+            <h1>Heading 1 (h1)<br> <small>Tagline</small></h1>
+        </div>
+        
+    </header>
+        
+    <!-- Columns -->
+    
+        <div class="container">
+            <h2>Heading 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        
+        <div class="container">
+            <h3>Heading 3</h3>
+             <ul>
+                <li>List item</li>
+                <li>List item</li>
+                <li>List item</li>
+            </ul>
+        </div>    
+       <div class="container">
+           <p>other content</p>
+           <p>1 2 3 4 5 6 7 8 9 10</p>
+        </div>
+</main>  
+      
+<footer>
+        
+    <?php include 'includes/footer_grid.html';?>
+    <?php include 'includes/footer.php';?>
+        
+</footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Simple Weather Plugin -->
+    <script src="js/jquery.simpleWeather.min.js"></script>
+    <!-- Moment.js for simple weather plugin and Social feed plugin -->
+    <script src="js/moment.js"></script>
+    <!-- jquery color for simple weather plugin -->
+    <script src="js/jquery.color.js"></script>
+    <!-- Ace in the Hole .js file -->
+    <script src="js/scripts.js"></script>
+    
+  </body>
+    
+</html>
